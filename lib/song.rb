7a566c116fc16f580c57b53d36007c80fffdbcd6
PepperMcLean <jackmcl206@gmail.com>
2021-02-28 04:45:38 -0600

    commit tree d38a5917b51530dbd29827065fde186050c76a6e

--- lib/song.rb
+++ lib/song.rb
@@ -1,4 +1,4 @@
-class Song
+ class Song
   attr_accessor :name, :artist_name
   @@all = []
 
@@ -10,4 +10,56 @@ class Song
     self.class.all << self
   end
 
+  def self.create
+    song = self.new
+    @@all << song
+    return song
+  end
+
+  def self.new_by_name(name)
+    song = self.new
+    song.name = name
+    song
+  end
+
+  def self.create_by_name(name)
+    song = self.new_by_name(name)
+    @@all << song
+    song
+  end
+
+  def self.find_by_name(name)
+    @@all.each_with_index do |title, index|
+      if title.name == name
+        return @@all[index]
+      end
+    end
+    return nil  
+  end
+
+  def self.find_or_create_by_name(name)
+    if self.find_by_name(name)
+      return self.find_by_name(name)
+    end
+    return self.create_by_name(name)
+  end
+
+  def self.alphabetical
+    @@all.sort_by {|song| song.name}
+  end
+
+  def self.new_from_filename(name)
+    song = self.new
+    song.artist_name = name.chomp('.mp3').split(' - ')[0]
+    song.name = name.chomp('.mp3').split(' - ')[1]
+    return song
+  end
+
+  def self.create_from_filename(name)
+    self.new_from_filename(name).save
+  end
+
+  def self.destroy_all
+    @@all = []
+  end
 end
